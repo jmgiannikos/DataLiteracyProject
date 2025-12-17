54a9af5073eb18a40d1f47b4bf59721302e1600f
Jan-Malte Giannikos <janmaltegiannikos@gmail.com>
2025-12-17 15:29:02 +0100

    expanded analysis code. Fixed issue in PCA where sentence stats were included, despite being a wildly different unit. Implemented plotting of PCA loadings to get more insights. Implemented individual word frequency distribution plotting. Found an abnormality in the data, that is likely due to data duplication during article processing

--- analysis.py
+++ analysis.py
@@ -43,11 +43,15 @@ def tsne_dim_reduction(data, perplexity=30):
         reduced_data = TSNE(n_components=2, learning_rate='auto', init='pca', perplexity=perplexity).fit_transform(data)
     return reduced_data
 
-def pca_dim_reduction(data):
-    reduced_data = PCA(n_components=2).fit_transform(data)
-    return reduced_data
+def pca_dim_reduction(data, axes=2):
+    pca = PCA(n_components=axes)
+    reduced_data = pca.fit_transform(data)
+    loadings = pca.components_
+    explained_variance = pca.explained_variance_
 
-def plot_dim_reduced_data(reduced_data, author_handles):
+    return reduced_data, loadings, explained_variance
+
+def plot_dim_reduced_data(reduced_data, author_handles, cat_offset=0):
     author_wise_data = {}
     for author_label in set(author_handles):
         author_mask = np.vectorize(lambda x: x == author_label)(author_handles)
@@ -56,21 +60,80 @@ def plot_dim_reduced_data(reduced_data, author_handles):
         assert author_entries.shape == (np.sum(author_mask), reduced_data.shape[1])
         author_wise_data[author_label] = author_entries
     
-    for author_label in author_wise_data.keys():
+    for i, author_label in enumerate(author_wise_data.keys()):
+        y_offset = i*cat_offset
         x = author_wise_data[author_label][:,0].T
-        y = author_wise_data[author_label][:,1].T
+        y = author_wise_data[author_label][:,1].T + y_offset
         plt.scatter(x, y)
     plt.show()
 
-def main():
-    print("###### PCA ######")
-    for csv_path in CSV_PATHS:
+def words_only(data_array):
+    return data_array[:, :-2]
+
+def get_topn_words(values, words):
+    sorted_zip = list(zip(values, words))
+    sorted_zip.sort(key=lambda x: abs(x[0]), reverse=True)
+    values = [tup[0] for tup in sorted_zip]
+    words = [tup[1] for tup in sorted_zip]
+    return values, words
+
+def plot_loadings(loadings, word_list, max_show=20):
+    for loading in loadings:
+        loading_1, word_list_1 = get_topn_words(loading, word_list)
+        plt.bar(word_list_1[:max_show], loading_1[:max_show])
+        plt.show()
+
+# adds randomized second dimension for plotting purposes
+def expand_to_2dim(data):
+    if len(np.shape(data)) == 1:
+        data = np.expand_dims(data,1)
+    random_ys = np.random.permutation(len(data))/len(data)
+    data = np.append(data, np.expand_dims(random_ys,1), axis=1)
+    return data
+
+def process_PCA(csv_paths=CSV_PATHS, dims=2):
+    for csv_path in csv_paths:
         data_df = load_csv(csv_path)
         author_handles, feature_labels, data = get_np_dataset(data_df)
         data = normalize_word_rows(data)
-        pca_reduced_data = pca_dim_reduction(data)
+        data = words_only(data) # fetch only words, so that all entries have the same unit (word frequency)
+        pca_reduced_data, loadings, explained_variance = pca_dim_reduction(data, axes=dims)
+        if dims == 1:
+            pca_reduced_data = expand_to_2dim(pca_reduced_data)
+            cat_offset = 1
+        else:
+            cat_offset = 0
+        print(explained_variance)
         print(f"CSV: {csv_path.split("/")[-1]}")
-        plot_dim_reduced_data(pca_reduced_data, author_handles)
+        plot_dim_reduced_data(pca_reduced_data, author_handles, cat_offset=cat_offset)
+        plot_loadings(loadings, feature_labels[:-2])
+
+def plot_word_frequency_distribution(dataframe, word):
+    author_handles, feature_labels, data = get_np_dataset(dataframe)
+    data = normalize_word_rows(data)
+    word_idx = np.where(feature_labels[:-2] == word)
+    data = np.squeeze(data[:,word_idx])
+    print(np.shape(data))
+    data = expand_to_2dim(data)
+    plot_dim_reduced_data(data, author_handles, cat_offset=1)
+
+def plot_word_frequency_distributions(words, csv_paths=CSV_PATHS):
+    for csv_path in csv_paths:
+        print(f"CSV: {csv_path.split("/")[-1]}")
+        data_df = load_csv(csv_path)
+        for word in words:
+            print(f"#### Distribution over '{word}' frequency ####")
+            plot_word_frequency_distribution(data_df, word)
+
+def main():
+    print("###### Top word distributions ######")
+    plot_word_frequency_distributions(words=["the", "we", "of", "that"])
+
+    print("###### PCA onto one Dim ######")
+    process_PCA(dims=1)
+    
+    print("###### PCA ######")
+    process_PCA()
 
     print("###### t-SNE ######")
     for csv_path in CSV_PATHS:
