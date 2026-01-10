304066cf45d489eb7a74400cae426545ecdd4804
Raphael Kopecki <147395635+captaintew@users.noreply.github.com>
2026-01-10 16:09:27 +0100

    Update main.py
    using import_from_txt instead

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from google_scholar_handler import parse_papers_page
 from arxiv_handler import search_arxiv, download_source
-from utils import strip_entry_id, sanitize_article_id, import_dataset, realize_path
+from utils import strip_entry_id, sanitize_article_id, import_dataset, realize_path, import_from_txt
 from tex_parsing import process_tex_source
 from csv_dataset_generator import generate_csv
 import csv
 
-HTML_DOC_PATH = "/home/jan-malte/DataLiteracyProject/AuthorPages"
+HTML_DOC_PATH = "/home/jan-malte/DataLiteracyProject/authortxts"
 CSV_FILE_PATHS= ["./union_pruned.csv", "./inter_pruned.csv", "./union_raw.csv", "./inter_raw.csv"] # NOTE: must have exactly 4 values, that are valid file paths
 
 def process_articles(articles, author_handle, verbose=True):
@@ -39,7 +39,7 @@ def process_articles(articles, author_handle, verbose=True):
                 # collect sentence outputs and handles
                 sentence_lists.append(sentences)
                 article_handles.append(f"{author_handle}/{strip_entry_id(sanitize_article_id(paper_arxiv_id))}")
-                
+
                 if verbose:
                     print(f"Processing article {i} with ID {paper_arxiv_id} successful!")
             except Exception as inst:
@@ -59,16 +59,15 @@ def process_articles(articles, author_handle, verbose=True):
 
 
 def main():
-    html_paper_pages, author_handles = import_dataset(HTML_DOC_PATH, file_types=["html"])
-    global_sentence_lists = [] 
+    author_paper_dict = import_from_txt(HTML_DOC_PATH)
+    global_sentence_lists = []
     global_article_handles = []
     
-    for i, html_page in enumerate(html_paper_pages):
-        articles = parse_papers_page(html_page)
-        sentence_lists, article_handles = process_articles(articles, author_handles[i])
+    for author in author_paper_dict:
+        articles = author_paper_dict[author]
+        sentence_lists, article_handles = process_articles(articles, author)
         global_sentence_lists = global_sentence_lists + sentence_lists
         global_article_handles = global_article_handles + article_handles
-
 
     print(global_article_handles)
     i = 0
@@ -78,5 +77,7 @@ def main():
             writer.writerows(csv_variant)
         i += 1
 
+
 if __name__ == "__main__":
     main()
+
